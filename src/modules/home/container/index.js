45ba605b476242041a23391db359a0829adfe800
avtarvikas <avtarvikas236@gmail.com>
2018-07-08 19:28:55 +0530

    Added login section

--- src/modules/home/container/index.js
+++ src/modules/home/container/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import logo from '../../../assets/svg/logo.svg';
 import Header from '../../../common/components/Header';
 import Banner from '../component/Banner';
+import LoginOptions from '../component/LoginOptions';
 
 class Home extends Component {
   render() {
@@ -9,13 +9,7 @@ class Home extends Component {
       <div className="container-fluid App">
         <Header />
         <Banner />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <LoginOptions />
       </div>
     );
   }
