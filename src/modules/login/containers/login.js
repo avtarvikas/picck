235f7823651006f70fee1a7b6f7039ef5830ab5e
avtarvikas <avtarvikas236@gmail.com>
2018-07-11 23:57:01 +0530

    login

--- src/modules/login/containers/login.js
+++ src/modules/login/containers/login.js
@@ -3,8 +3,23 @@ import React, { Component } from "react";
 class Login extends Component {
   render() {
     return (
-      <div>
-        <h1>login</h1>
+      <div className="container login">
+        <div class="outer">
+          <form class="form-group">
+            <img src="" />
+            <input type="text" name="username" placeholder="Username" />
+            <input type="password" name="password" placeholder="Password" />
+            <button type="submit" class="btn-primary">Login</button>
+          </form>
+          <div class="row icons">
+            <img src="if_Gplus.svg" />
+            <img src="if_Facebook.svg" />
+            <img src="if_LinkedIn.svg" />
+            <img src="if_Facebook.svg" />
+            <img src="if_Twitter.svg" />
+            <img src="if_Instagram.svg" />
+          </div>
+        </div>
       </div>
     );
   }
