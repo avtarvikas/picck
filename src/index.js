94d9e30f37fe4b71da5f1cf56db9d80cd53ee4cc
InfinityCoders <infiniticoder@gmail.com>
2018-07-25 23:24:11 +0530

    fixed router and signup login

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const store = createStore(
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
+    <Router forceRefresh={true}>
       <DeviceUtil>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/login" component={Login} />
